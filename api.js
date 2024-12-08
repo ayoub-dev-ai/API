7a6805f27e73eb04708778fb0057affb131c830f
ayoub-dev-ai <hakremafia27@gmail.com>
2024-12-08 14:22:52 +0100

    Update api.js

--- api.js
+++ api.js
@@ -10,10 +10,10 @@ router.get("/gpt4", (req, res) => {
     // يعني بدل تكتب الجملة ذي كل مرة req.query.q || req.query.query 
     //تكتب text وفقط
 
-    if(!text) return res.send({status: false, owner: '@إسمك', err: 'اكتب شيئا !'});
+    if(!text) return res.send({status: false, owner: '@ayoub.eeea', err: 'اكتب شيئا !'});
   // if(!text) تعني في حالة ما كتب شي ترسل له السطر ذا الي تقول فيه اكتب شي
 
-    axios.get('https://api.bk9.site/ai/gpt4?q='+text)
+    axios.get('https://api.bk9.site/ai/gemini?q='+text)
         .then((response) => {
             const responseData = response.data; //هنا يأخذ المعلومات من الرابط فوق ويحطهم عندك
             res.send({status: true, owner: '@ayoub.eeea', BK9: responseData});
